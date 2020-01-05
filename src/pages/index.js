b9cc6c7a0c816be53e18082533aa33a464b74c24
Jack Leslie <jjdl@st-andrews.ac.uk>
2020-01-05 18:07:02 +0000

    New design template

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,41 @@
 import React from "react"
-import { Box, Button, Heading, Text, useColorMode } from "@chakra-ui/core"
+import {
+  Box,
+  Stack,
+  Input,
+  FormControl,
+  FormLabel,
+  FormHelperText,
+  Heading,
+  Button,
+} from "@chakra-ui/core"
 
-const IndexPage = () => {
-  const { colorMode, toggleColorMode } = useColorMode()
-
+function IndexPage() {
   return (
-    <Box p={5}>
-      <Heading size="md" mb={5}>
-        Hello World
-      </Heading>
-      <Text mb={3}>Color mode: {colorMode}</Text>
-      <Button variantColor="teal" onClick={toggleColorMode}>
-        Toggle color mode
-      </Button>
+    <Box p={8}>
+      <Heading mb={4}>Latin Diachronic Frontend</Heading>
+      <Stack spacing={3}>
+        <FormControl>
+          <FormLabel htmlFor="author">Author</FormLabel>
+          <Input
+            type="author"
+            id="author"
+            aria-describedby="author-helper-text"
+          />
+          <FormHelperText id="author-helper-text">
+            Enter as many authors as you'd like, or leave blank to search all
+            authors.
+          </FormHelperText>
+        </FormControl>
+        <FormControl>
+          <FormLabel htmlFor="lemma">Lemma</FormLabel>
+          <Input type="lemma" id="lemma" aria-describedby="lemma-helper-text" />
+          <FormHelperText id="lemma-helper-text">
+            Enter a lemma or a wordform.
+          </FormHelperText>
+        </FormControl>
+      </Stack>
+      <Button mt={5}>Search</Button>
     </Box>
   )
 }
