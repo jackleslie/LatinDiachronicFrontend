17fae440b9ac795085f629bc228771a032dc3ca3
Jack Leslie <jjdl@st-andrews.ac.uk>
2020-02-03 01:34:57 +0000

    Fix prod lambda

--- lambda/phi/phi.js
+++ lambda/phi/phi.js
@@ -1,35 +1,27 @@
 const chromium = require("chrome-aws-lambda")
 
 exports.handler = async function(event, context) {
+  let result = null
+  let browser = null
   try {
     const { line } = await event.queryStringParameters
-
-    const isProduction = process.env.NODE_ENV === "production"
-    const launchOptions = isProduction
-      ? {
-          args: chromium.args,
-          defaultViewport: chromium.defaultViewport,
-          executablePath: await chromium.executablePath,
-          headless: chromium.headless,
-        }
-      : undefined
-
-    const browser = await chromium.puppeteer.launch(launchOptions)
+    browser = await chromium.puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless,
+    })
     const page = await browser.newPage()
     await page.goto(`https://latin.packhum.org/search?q=${line}`, {
       waitUntil: ["domcontentloaded", "networkidle0"],
     })
     const link = await page.$eval("#match .tlink", elem => elem.innerText)
     const extract = await page.$eval("#match .slink", elem => elem.innerHTML)
-    const result = {
+    result = {
       link,
       extract,
     }
     await browser.close()
-    return {
-      statusCode: 200,
-      body: JSON.stringify(result),
-    }
   } catch (err) {
     console.log(err)
     return {
@@ -38,5 +30,14 @@ exports.handler = async function(event, context) {
         msg: "Please try again later, or use a separate reference tool.",
       }),
     }
+  } finally {
+    if (browser !== null) {
+      await browser.close()
+    }
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(result),
   }
 }
